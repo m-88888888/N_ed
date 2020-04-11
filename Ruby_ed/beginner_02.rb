304c9eaa2b0878770c9cbb289d0ff9455c8982dc
m-88888888 <hachi8810@gmail.som>
2020-04-11 19:15:26 +0900

    四則演算を使って効率化

--- Ruby_ed/beginner_02.rb
+++ Ruby_ed/beginner_02.rb
@@ -1,20 +1,26 @@
-require 'benchmark'
+require "benchmark"
+#
+#def fib(n)
+#  return n if n == 0
+#  return n if n == 1
+#  fib(n - 1) + fib(n - 2)
+#end
+#
+#length = 40
+#result = Benchmark.realtime do
+#  length.times do |i|
+#    p fib i
+#  end
+#end
+#
+#p "処理概要 #{result}s"
 
-def fib(n)
-  if (n == 0)
-    return 0 
-  elsif(n == 1)
-    return 1 
-  end
-  fib(n-1) + fib(n-2)
+def fibonacci4(n)
+  return if n < 0
+  return n if n < 2
+  a, b = 0, 1
+  n.times { a, b = b, a + b }
+  a
 end
 
-length = 40
-
-result = Benchmark.realtime do
-  length.times do |i|
-    p fib(i)
-end
-end
-
-p "処理概要 #{result}s"
+p fibonacci4 1000 ** 2
